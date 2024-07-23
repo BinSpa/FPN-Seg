ece490e10053b9c263cb1a07efcefddd1360e229
BinSpa <luogu1024116558@gmail.com>
2024-07-23 21:40:07 +0800

    add FBP

--- data/__init__.py
+++ data/__init__.py
@@ -1,4 +1,4 @@
-from data import Cityscapes, URUR, GID
+from data import Cityscapes, URUR, GID, FBP
 from torch.utils.data import DataLoader
 
 def make_data_loader(args, **kwargs):
@@ -30,6 +30,12 @@ def make_data_loader(args, **kwargs):
         test_loader = None
         return train_loader, val_loader, test_loader, num_class
     elif args.dataset == 'FBP':
-        pass
+        train_set = FBP.FBPSegmentation(args, split='train')
+        val_set = FBP.FBPSegmentation(args, split='val')
+        num_class = train_set.NUM_CLASSES
+        train_loader = DataLoader(train_set, batch_size=args.batch_size, shuffle=True, **kwargs)
+        val_loader = DataLoader(val_set, batch_size=1, shuffle=False, **kwargs)
+        test_loader = None
+        return train_loader, val_loader, test_loader, num_class
     else:
         raise NotImplementedError
